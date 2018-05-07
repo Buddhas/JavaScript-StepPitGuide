48df38707ed08ea190f301f41da1c16f50d516ec
Buddhas <2424932515@qq.com>
2018-05-07 20:27:15 +0800

    node模拟ajax跨域请求

---
+++ 13/node.js模拟ajax请求/ajax.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var url = require('url');
+var createServer = http.createServer(onRequest);
+
+
+
+
+
+function onRequest(request, response) {
+    response.writeHead(200, {
+        'Content-Type': 'text/plain',
+        'Access-Control-Allow-Origin': '*'
+    });
+    var str = JSON.stringify(url.parse(request.url, true).query);
+    response.write('我是');
+    response.end();
+}
+function test(){
+    console.log('111111111222222222');
+}
+createServer.listen(8080);
+console.log('Server running  at http://127.0.0.1:8080/');
