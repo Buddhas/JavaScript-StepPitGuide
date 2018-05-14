8a06c6c5796cd08c35f47f72aaac61f62b372301
Buddhas <2424932515@qq.com>
2018-05-14 15:00:56 +0800

    函数节流函数防抖

---
+++ 12/函数节流.js
@@ -0,0 +1,14 @@
+
+
+//函数节流指的是函数在一段时间内只能执行一次
+function throttle(func,delay){
+    var timer = null
+    return function(){
+        if(!timer){
+            timer = setTimeout(() => {
+                func.call(this,arguments)
+                timer = null//重点
+            }, delay);
+        }
+    }
+}
---
+++ 12/函数防抖.js
@@ -0,0 +1,13 @@
+//函数防抖指的是在操作频繁触发的情况下，只有任务触发的时间间隔超过指定的间隔的时候，任务才会执行
+
+
+function debounce(fn,delay){
+    let timeout = null
+   
+    return function(){
+        clearTimeout(timeout)
+       setTimeout(() => {
+            fn.call(this,arguments)//箭头函没有this，绑定的是settimeOUt的this，即是window
+        }, 1000);
+    }
+}
