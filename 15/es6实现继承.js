bddfc933aec52cc12109726c8536c94ac17fcc11
Buddhas <2424932515@qq.com>
2018-05-14 14:17:00 +0800

    es6实现继承

---
+++ 15/es6实现继承.js
@@ -0,0 +1,21 @@
+class lol{
+   
+    constructor(people,species){
+        this.people = people;
+        this.species = species;
+    }
+    toString(){
+        return '('+this.people+','+this.species+')';
+    }
+}
+
+class hero extends lol{
+    constructor(people,species,age){
+        super(people,species)//es6必须先调用super才能用this,这里实现了继承
+        this.age = age
+    }
+}
+
+var people = new hero('德玛西亚','大宝剑','26')
+
+console.log(people.toString())
