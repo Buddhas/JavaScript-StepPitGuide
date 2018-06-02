edeaea770c76eafb007bedb508cececc2779964b
Buddhas <2424932515@qq.com>
2018-06-02 21:48:27 +0800

    快速排序

---
+++ 17/快速排序.js
@@ -0,0 +1,20 @@
+function sort(arr){
+    if(arr.length == 0){
+        return []
+    }else{
+        var left = []
+        var right = []
+        var tmp = arr[0]
+        for(var i = 1; i < arr.length; i++){
+            if(tmp > arr[i]){
+                left.push(arr[i]);
+            }else{
+                right.push(arr[i]);
+            }
+        }
+        return sort(left).concat(tmp,sort(right))
+    }
+
+}
+
+console.log(sort([5,2,4,10,3,3]));
