61462bb34d8996ae8df0a44c383e7494a4c9bce5
Buddhas <2424932515@qq.com>
2018-07-13 15:20:52 +0800

    手动实现一个bind

---
+++ 22/手动实现bind.js
@@ -0,0 +1,44 @@
+Function.prototype.bind2 = function (context) {
+    var self = this;
+    console.log("11111111111")
+    console.log(self)
+    var args = Array.prototype.slice.call(arguments, 1);
+
+    var fbound = function () {
+
+        var bindArgs = Array.prototype.slice.call(arguments);
+        console.log("222222222")
+        console.log(this)
+        // 当作为构造函数时，this 指向实例，self 指向绑定函数，因为下面一句 `fbound.prototype = this.prototype;`，已经修改了 fbound.prototype 为 绑定函数的 prototype，此时结果为 true，当结果为 true 的时候，this 指向实例。
+        // 当作为普通函数时，this 指向 window，self 指向绑定函数，此时结果为 false，当结果为 false 的时候，this 指向绑定的 context。
+        self.apply(this instanceof self ? this : context, args.concat(bindArgs));
+    }
+    // 修改返回函数的 prototype 为绑定函数的 prototype，实例就可以继承函数的原型中的值
+    fbound.prototype = this.prototype;
+    return fbound;
+}
+
+var value = 2;
+
+var foo = {
+    value: 1
+};
+
+function bar(name, age) {
+    this.habit = 'shopping';
+    console.log(this.value);
+    console.log(name);
+    console.log(age);
+}
+
+bar.prototype.friend = 'kevin';
+
+
+var bindFoo = bar.bind2(foo, 'daisy');
+
+var obj = new bindFoo('18');
+// undefined
+// daisy
+// 18
+console.log(obj.habit);
+console.log(obj.friend);
