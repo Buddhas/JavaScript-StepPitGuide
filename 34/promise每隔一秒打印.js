0cea906eab9564f3e53f751ad926bf239e06255d
Buddhas <2424932515@qq.com>
2021-01-13 11:12:58 +0800

    feat: promise每隔一秒打印

--- 34/promise每隔一秒打印.js
+++ 34/promise每隔一秒打印.js
@@ -1,1 +1,30 @@
-1
+
+// 方式一
+function printPorimse(num, current) {
+  if (current < num) {
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        console.log(current+1)
+        resolve()
+      }, 1000);
+    }).then(() => {
+      printPorimse(num, ++current)
+    })
+  }
+}
+
+printPorimse(3,0)
+
+
+
+// 方式二
+[1,2,3].reduce((p, r) => {
+  return p.then(() => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        console.log(r)
+        resolve()
+      }, 1000);
+    })
+  })
+}, Promise.resolve())
